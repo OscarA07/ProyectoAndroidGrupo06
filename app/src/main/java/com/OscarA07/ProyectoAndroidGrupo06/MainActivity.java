771c69207835e9289662ba72ecf3ef8b2c49f263
Oscar <oscaracegar07@gmail.com>
2021-10-29 10:54:34 +0200

    Activación de submenu

--- app/src/main/java/com/OscarA07/ProyectoAndroidGrupo06/MainActivity.java
+++ app/src/main/java/com/OscarA07/ProyectoAndroidGrupo06/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -31,6 +33,30 @@ public class MainActivity extends AppCompatActivity {
         newIntent.putExtra("year", 2020);
         startActivity(newIntent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //cuando el usuario oprime en el submenu para ir a la segunda aplicacion
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        int id = menuItem.getItemId(); //codigo de lo que toco el ususario
+        if (id == R.id.mnu_second_activity) {
+            Intent newIntent = new Intent(this, SecondActivity.class);
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            newIntent.putExtra("msg","Hola MinTIC");
+            newIntent.putExtra("year", 2020);
+            startActivity(newIntent);
+        }
+        else if (id == R.id.mnu_third_activity) {
+            Intent newIntent = new Intent(this, ThirdActivity.class);
+            startActivity(newIntent);
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
 
     @Override
     protected void onResume() {
--- app/src/main/java/com/OscarA07/ProyectoAndroidGrupo06/ThirdActivity.java
+++ app/src/main/java/com/OscarA07/ProyectoAndroidGrupo06/ThirdActivity.java
@@ -3,6 +3,7 @@ package com.OscarA07.ProyectoAndroidGrupo06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        //Activar el supoorte para la actividad
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    //Destruir la aplicación o actividad
+    public void onBackPressed(){
+        finish();
+    }
+
+    //cuando el usuario oprime algun boton del celular
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        int id = menuItem.getItemId(); //codigo de lo que toco el ususario
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
 }
